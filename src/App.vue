46faf41960c29965ceacbc41d8601ff372f03daf
Jiaxin <jiaxinnc@gmail.com>
2025-03-19 22:28:41 +0800

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -3,16 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl">
+    Vue + Vite + TailwindCSS Setup Complete!
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
 
 <style scoped>
 .logo {
